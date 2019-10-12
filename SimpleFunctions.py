becf7d7f3de6277f293734941cbb73e3e0ee84c2
Dejameshae <daemeshae@yahoo.com>
2019-10-12 15:23:02 -0500

    added Functions script

---
+++ SimpleFunctions.py
@@ -0,0 +1,6 @@
+# CLT + ALT + N can also run your code
+# To run you code in the termal to accept an input CTL ~ and copy path from the above step if the path is not already there
+
+name = "Deja"
+print(len(name))
+# len function will print out the length of the string wich is 4
--- StartPython.py
+++ StartPython.py
@@ -1,25 +1,23 @@
 '''print("Deja")
-CLT + ALT + N can also run your code'''
+#CLT + ALT + N can also run your code
+#To run you code in the termal to accept an input CTL ~ and copy path from the above step if the path is not already there
 
-'''students_count = 1000
+students_count = 1000
 print(students_count)
-
-name = "Deja"
-print(len(name))
 
 print(name[0])  # To get a certain index
 print(name[0:3])'''  # slice a string
 
-'''first = "deja"
+first = "deja"
 last = "ameringer"
-full = f"{first} {last}"  # formatted string but still a lil confused
-print(full)
+full_name = f"Welcome {first} {last}"  # the f means its a formatted string
+print(full_name)
 
 # dotnotation methods
-print(full.title())  # capatizle the first letter
-print(full.strip())  # to remove white space'''
+'''print(full.title())  # capatizle the first letter
+print(full.strip())  # to remove white space
 
-'''x = input("x: ")
+x = input("x: ")
 y = int(x) + 1
 print(f"x: {x}, y: {y}")
 # the above take in an input (x) then adds 1 to to it'''
@@ -32,12 +30,14 @@ print("The current date and time is: ")
 print(now.strftime("%b-%d-%Y %H:%M"))'''
 # datetime.now() to get current date and time. Then strftime() method creates a string representing date and time in another format
 
+# Import is like importing an entire library like import
+'''import this 
+
 # Tuples are similar to lists but they are immutable - which means they cannot be changed after creation
-import this
 my_tuple = ("Deja", "Ameringer", 27, "Associate Engineer")
 print(my_tuple)
 
 # Dictionaries are a type of associative array (an object) implemented using hash tables containing key/value pairs. They are unordered.
 my_dict = {"Key 1": "Value 1", "name": "Deja Ameringer", "pi": 3.14}
 print(my_dict["name"])
-# key being name and its value is Deja Ameringer
+# key being name and its value is Deja Ameringer'''
