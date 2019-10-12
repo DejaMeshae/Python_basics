f979c6b6e8f830aeb684f68c84587dbcf9565f24
Dejameshae <daemeshae@yahoo.com>
2019-10-11 23:11:41 -0500

    initial push

---
+++ app.py
@@ -0,0 +1,31 @@
+'''print("Deja")
+CLT + ALT + N can also run your code'''
+
+'''students_count = 1000
+print(students_count)
+
+name = "Deja"
+print(len(name))
+
+print(name[0])  # To get a certain index
+print(name[0:3])'''  # slice a string
+
+'''first = "deja"
+last = "ameringer"
+full = f"{first} {last}"  # formatted string but still a lil confused
+print(full)
+
+# dotnotation methods
+print(full.title())  # capatizle the first letter
+print(full.strip())  # to remove white space'''
+
+x = input("x: ")
+y = int(x) + 1
+print(f"x: {x}, y: {y}")
+# the above take in an input (x) then adds 1 to to it'''
+
+'''import datetime
+now = datetime.datetime.now()
+x = input("Ask Siri what's the date and time:")
+print("The current date and time is: ")
+print(now.strftime("%m-%d-%Y %H:%M"))'''
