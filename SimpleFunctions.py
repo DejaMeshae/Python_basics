585f72ff63b4408b19aee16b316816fd4a85042e
Dejameshae <daemeshae@yahoo.com>
2019-10-29 23:45:58 -0500

    added files file

--- SimpleFunctions.py
+++ SimpleFunctions.py
@@ -1,11 +1,12 @@
 # CLT + ALT + N can also run your code
 # To run you code in the termal to accept an input CTL ~ and copy path from the above step if the path is not already there
 
+# functions
 name = "Deja"
 print(len(name))
 # len function will print out the length of the string wich is 4
 
-# Dotnotation methods
+# Dotnotation
 # .title() capatizle the first letter
 sentence = "girl bye"
 print(sentence.title())
