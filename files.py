24636d435e7cea32836b77b6e2d150c60fbdbc99
Dejameshae <daemeshae@yahoo.com>
2019-10-30 19:34:29 -0500

    created a new .txt file names new_days

--- files.py
+++ files.py
@@ -1,14 +1,28 @@
-# path = 'days.txt'
-'''line 1 specify the path'''
+# SHORTCUTS -> CTL+ALT+N = run code in output, right click run code in terminal
 
-# days_file = open(path, 'r')
+path = 'c:/users\daeme\OneDrive\Desktop\Personal Projects\Python_basics\days.txt'
+# line 1 specify the path
+
+days_file = open(path, 'r')
 '''line 4 set a variable and calls the open () function and pass in the path
 variable I created above, the r is a mode and is used for reading'''
 
-# days_file.readline()
+days = days_file.read()
+print(days)
+# line 10 & 11 i created a variable that reads the file and pass it in the print funtion
+
+'''days = days_file.readline()
+# readline will return whats in the first line only, but if i call it again it will then return the second line
+
+days = days_file.readlines()
+# readlines return a list of the lines in a file where each item are all on one line'''
+
+title = 'Days of the Week\n'
+new_path = 'c:/users\daeme\OneDrive\Desktop\Personal Projects\Python_basics\new_days.txt'
+new_days = open(new_path, 'W')
 
 
-f = open("c:\VSStuff//delete.txt", "a")
+'''f = open("c:\VSStuff//delete.txt", "a")
 # line 12 open/create a file to this path (I can change the path if i want, this path I got from the video and I created a VSStuff folder)
 # Then I can go to this temp file and open it up and sure enough my delete.txt file is there!
 
@@ -27,4 +41,4 @@ print("Total number of lines is:", count)
 
 with open("c:\VSStuff//delete.txt", 'r') as f:
     f_contents = f.read()
-    print(f_contents)
+    print(f_contents)'''
